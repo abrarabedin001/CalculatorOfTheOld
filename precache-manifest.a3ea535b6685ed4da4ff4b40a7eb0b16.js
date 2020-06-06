6c883d632c2b2496a105a261f9d32f28543ebf80
abrarabedin001 <abrarabedin001@gmail.com>
2020-06-07 01:24:31 +0600

    Updates

---
+++ precache-manifest.a3ea535b6685ed4da4ff4b40a7eb0b16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d354ae2ae111e6b92c6e67570bf812a",
+    "url": "/CalculatorOfTheOld/index.html"
+  },
+  {
+    "revision": "370c4df33b1aad5ecb5b",
+    "url": "/CalculatorOfTheOld/static/css/main.c47b8081.chunk.css"
+  },
+  {
+    "revision": "2dca12adbc79a0c68de9",
+    "url": "/CalculatorOfTheOld/static/js/2.cc11b56d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CalculatorOfTheOld/static/js/2.cc11b56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370c4df33b1aad5ecb5b",
+    "url": "/CalculatorOfTheOld/static/js/main.7b1bfa84.chunk.js"
+  },
+  {
+    "revision": "70906367f6a0d4195252",
+    "url": "/CalculatorOfTheOld/static/js/runtime-main.a0d0a295.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/CalculatorOfTheOld/static/media/digital-7.regular.1e670d88.ttf"
+  }
+]);
